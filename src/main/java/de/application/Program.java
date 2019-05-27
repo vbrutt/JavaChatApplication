6c996a5b137173cf143af5487890cf67de12f78d
Sebastian Kaiser <sebastian.kaiser@yahoo.com>
2019-05-27 20:39:16 +0200

    Rearranged files

---
+++ src/main/java/de/application/Program.java
@@ -0,0 +1,13 @@
+package TestPackage;
+
+public class Program {
+    public static void main(String[] args) {
+        try {
+            TestProject testProject = new TestProject();
+            testProject.doStuff();
+            testProject.dispose();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
